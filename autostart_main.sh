c4463192bbe233b827d40fa26ba7536c3b397933
kodi <samko.chudy@gmail.com>
2025-11-06 17:50:31 +0100

    new version

--- autostart_main.sh
+++ autostart_main.sh
@@ -6,16 +6,18 @@ cd /home/truevar/Documents/TrueVAR || exit 1
 xrandr --output HDMI-1 --mode 1920x1080 --same-as DP-1
 xrandr --output HDMI-2 --mode 1920x1080 --same-as DP-1
 
-# Check if GitHub is reachable
-# if ping -c 1 github.com &> /dev/null; then
-#     echo "Internet detected, updating code..."
-#     git pull origin main || echo "Update failed, continuing with local code"
-# else
-#     echo "No internet connection, skipping update"
-# fi
+Check if GitHub is reachable
+if ping -c 1 github.com &> /dev/null; then
+    echo "Internet detected, updating code..."
+    git pull origin main || echo "Update failed, continuing with local code"
+else
+    echo "No internet connection, skipping update"
+fi
 
 # Activate virtual environment
 source .venv/bin/activate
+
+exec pip install -r requirements.txt
 
 # Run your script
 exec python main.py
